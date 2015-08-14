99d7d626d126f1428aeb11e9aad72e601788cb7c
lichengf <1207813152@qq.com>
2015-08-14 12:03:36 +0800

    Buildings.java

---
+++ src/com/lichengfeng/chapterone/sectiontwo/Buildings.java
@@ -0,0 +1,25 @@
+package com.lichengfeng.chapterone.sectiontwo;
+
+import java.util.Scanner;
+
+public class Buildings {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		while(num > 0) {
+			int totalGreen = 0;
+			int N = sc.nextInt();
+			int M = sc.nextInt();
+			for (int i = 0 ; i < N ; i++) {
+				for (int j = 0 ; j < M ; j++) {
+					if(sc.nextInt() == 1) {
+						totalGreen++;
+					}
+				}
+			}
+			num--;
+			System.out.println(totalGreen);
+		}
+	}
+}
